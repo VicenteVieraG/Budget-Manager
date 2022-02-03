39ce9431af5db9d3dbb646cc1400b0051c01070e
Vicente Viera <plantedcoot706@gmail.com>
2022-02-03 16:54:15 -0600

    Modal form implemented and casting data to App

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -2,14 +2,33 @@ import { useState } from 'react'
 import Header from './components/Header'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 import Modal from './components/Modal'
+import {generarId} from './helpers'
 
 function App() {
 	const [presupuesto,setPresupuesto] =useState(0);
 	const [isValidPresupuesto,setIsValidPresupuesto]=useState(false);
 	const [modal, setModal] =useState(false)
+	const [animarModal, setAnimarModal] =useState(false)
+	const [gastos, setGastos]=useState([])
 
 	const handleNuevoGasto = () =>{
 		setModal(true)
+
+		setTimeout(() => {
+			setAnimarModal(true);
+		},500)
+	}
+
+
+	const guardarGasto = (gasto) =>{
+		gasto.id=generarId()
+		setGastos([...gastos,gasto])
+
+		setAnimarModal(false)
+
+		setTimeout(() => {
+			setModal(false)
+		},500)
 	}
 
 	return (
@@ -31,7 +50,13 @@ function App() {
 				</div>
 			)}
 
-			{modal && <Modal setModal={setModal}/>}
+			{modal && <Modal 
+						setModal={setModal} 
+						animarModal={animarModal} 
+						setAnimarModal={setAnimarModal}
+						guardarGasto={guardarGasto}
+						/>
+			}
 		</div>
 	)
 }
--- control-gastos/src/components/Modal.jsx
+++ control-gastos/src/components/Modal.jsx
@@ -1,8 +1,35 @@
 import CerrarBtn from "../img/cerrar.svg";
+import Mensaje from "./Mensaje.jsx"
+import {useState} from "react";
 
-const Modal = ({setModal}) => {
+const Modal = ({setModal,animarModal,setAnimarModal,guardarGasto}) => {
+	const [nombre, setNombre]=useState("");
+	const [cantidad, setCantidad]=useState(0);
+	const [categoria, setCategoria]=useState("");
+	const [mensaje, setMensaje] = useState("");
+
 	const ocultarModal= () => {
-		setModal(false)
+		setAnimarModal(false)
+
+		setTimeout(() => {
+			setModal(false)
+		},500)
+	}
+
+	const handleSubmit= (e) =>{
+		e.preventDefault();
+
+		if([nombre, cantidad, categoria].includes("")){
+			setMensaje("Todos los Campos Son Obligatorios")
+
+			setTimeout(() => {
+				setMensaje("");
+			},3000)
+
+			return;
+		}
+
+		guardarGasto({nombre,cantidad,categoria})
 	}
 
   return (
@@ -16,6 +43,61 @@ const Modal = ({setModal}) => {
 				
 			/>
 		</div>
+
+		<form
+			onSubmit={handleSubmit} 
+			className={`formulario ${animarModal ? "animar" : "cerrar"}`}
+		>
+			<legend>Nuevo Gasto</legend>
+
+			{mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+
+			<div className="campo">
+				<label htmlFor="nombre">Nombre Gasto</label>
+				<input
+					id="nombre"
+					type="text"
+					placeholder="Añade el Nombre del Gasto"
+					value={nombre}
+					onChange={e => setNombre(e.target.value)}
+				/>
+			</div>
+			
+			<div className="campo">
+				<label htmlFor="cantidad">Canidad</label>
+				<input
+					id="cantidad"
+					type="number"
+					placeholder="Añade la Cantidad del Gasto: ej. 300"
+					//value={cantidad}
+					onChange={e => setCantidad(Number(e.target.value))}
+				/>
+			</div>
+
+			<div className="campo">
+				<label htmlFor="categoria">Canidad</label>
+				
+				<select 
+					id="categoria"
+					value={categoria}
+					onChange={e => setCategoria(e.target.value)}
+				>
+					<option value="Seleccione">-- Seleccione --</option>
+					<option value="Ahorro">Ahorro</option>
+					<option value="Comida">Comida</option>
+					<option value="Casa">Casa</option>
+					<option value="Gastos">Gasto Varios</option>
+					<option value="Ocio">Ocio</option>
+					<option value="Salud">Salud</option>
+					<option value="Suscripciones">Suscripciones</option>
+				</select>
+			</div>
+
+			<input
+				type="submit"
+				value="Añadir Gasto"
+			/>
+		</form>
 	</div>
 	);
 };
